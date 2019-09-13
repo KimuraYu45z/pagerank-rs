def44c255d1723f20fc8e8f575e2f0cbad8ecfce
unknown <33382781+YuKimura45z@users.noreply.github.com>
2019-09-13 21:50:30 +0900

    [update]

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,18 @@ pub type Vector = HashMap<String, f64>;
 pub fn get_stochastic_matrix(link_matrix: &Matrix) -> Matrix {
   let mut stochastic_matrix = Matrix::new();
 
-  for (src, column) in link_matrix {
+  for (src, vector) in link_matrix {
     let mut sum = 0f64;
-    for (_, value) in column {
+    for (_, value) in vector {
       sum += value;
     }
 
-    stochastic_matrix.insert(src.to_string(), Vector::new());
-    for (dst, value) in column {
-      let v = stochastic_matrix.get_mut(src).unwrap();
+    let mut v = Vector::new();
+    
+    for (dst, value) in vector {
       v.insert(dst.to_string(), *value / sum);
     }
+    stochastic_matrix.insert(src.to_string(), v);
   }
 
   return stochastic_matrix;
@@ -25,14 +26,50 @@ pub fn get_stochastic_matrix(link_matrix: &Matrix) -> Matrix {
 pub fn transition_score(stochastic_matrix: &Matrix, current_score_vector: &Vector) -> Vector {
   let mut score = Vector::new();
 
-  for (src, column) in stochastic_matrix {
-    for (dst, value) in column {
-      score.entry(dst.to_string()).or_insert(0f64);
-      let current_score = current_score_vector.get(src).unwrap_or(&0f64);
-      let v = score.get_mut(dst).unwrap();
-      *v += *current_score * *value;
+  for (src, vector) in stochastic_matrix {
+    for (dst, value) in vector {
+      let dst_current_score = score.get(dst).unwrap_or(&0f64);
+      let src_current_score = current_score_vector.get(src).unwrap_or(&0f64);
+      &score.insert(dst.to_string(), *dst_current_score + *src_current_score * *value);
     }
   }
 
   return score;
+}
+
+#[test]
+fn test_pagerank() {
+  let mut link_matrix = Matrix::new();
+  const SIZE: i32 = 10;
+  for i in 0..SIZE {
+    let mut vector = Vector::new();
+
+    for j in 0..SIZE {
+      let surplus3 = (i + j + 1) as f64 % 3f64;
+      if surplus3 == 0f64 {
+        continue;
+      }
+      let surplus5 = (i + j + 1) as f64 % 5f64;
+
+      vector.insert(j.to_string(), surplus3 / surplus5);
+    }
+    link_matrix.insert(i.to_string(), vector);
+  }
+
+  let stochastic_matrix = get_stochastic_matrix(&link_matrix);
+
+  let mut score_vector = Vector::new();
+  const DEFAULT: f64 = 1f64 / SIZE as f64;
+  for i in 0..SIZE {
+    score_vector.insert(i.to_string(), DEFAULT);
+  }
+
+  const PRECISION: i32 = 6;
+  for _ in 0..PRECISION {
+    score_vector = transition_score(&stochastic_matrix, &score_vector);
+  }
+
+  for i in 0..SIZE {
+    println!("{}", score_vector[&i.to_string()]);
+  }
 }
